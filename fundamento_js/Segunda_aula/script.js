523896cafd141a3231bbd8b7f2e39bb009fa1d4d
Matheus_barret0 <matheusbarreto.dev@gmail.com>
2024-04-27 17:48:04 -0300

    Adicionando novas aulas

---
+++ fundamento_js/Segunda_aula/script.js
@@ -0,0 +1,20 @@
+//Var, let e const
+
+//var altura = 5;
+//var comprimento 7;
+
+//area = altura * comprimento;
+//console.log(area)
+
+let altura = 5;
+let comprimento 7;
+let forma = 'retangulo';
+let area; //precisa ser declarado antes de ser usada
+
+if (formula === 'retangulo'){
+    area = altura * comprimento;
+} else {
+    area = altura * comprimento / 2;
+}
+
+area = altura *
